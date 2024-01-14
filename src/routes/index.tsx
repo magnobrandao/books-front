8484ef6902659b4d9bb9a7ddefb017e3c8e9d78c
magno <magnoboc@gmail.com>
2024-01-14 18:17:37 -0300

    feat: card book

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 
-import { Button } from '@mui/material'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import CardBook from '../shared/components/Card-book'
 
 function AppRoutes() {
     return (
         <Routes>
-            <Route path='/home' element={<Button variant='contained' color='primary'>HOME</Button>} />
+            <Route path='/home' element={<CardBook title='titulo do livro' />} />
             <Route path='*' element={<Navigate to="/home" />} />
         </Routes>
 
---
+++ src/shared/components/Card-book/index.tsx
@@ -0,0 +1,41 @@
+import { Grid, Typography, Button } from "@mui/material";
+
+type TProps = {
+    title: string;
+    url?: string;
+
+};
+
+export default function CardBook({ title, url }: TProps) {
+    return (
+        <Grid
+            container
+            direction="column"
+            maxWidth={220}
+            minHeight={280}
+            alignItems="center"
+            gap={1}
+            boxShadow="0px 0px 4px 2px rgba(0, 0, 0, 0.28)"
+            borderRadius="8px"
+        >
+            <Grid item width={160} height={214} borderRadius="4px">
+                <img width="100%" height="100%" src="https://m.media-amazon.com/images/I/51nNwwVSclL._SY425_.jpg" alt="Book Cover" />
+            </Grid>
+
+            <Grid item container direction="column" alignItems="center" gap={1} mb={2}>
+                <Typography fontSize={21} fontWeight="500">
+                    {title}
+                </Typography>
+
+                <Button
+                    onClick={() => window.open(url, "_blank")}
+                    variant="contained"
+
+
+                >
+                    VER DETALHES
+                </Button>
+            </Grid>
+        </Grid>
+    );
+}
